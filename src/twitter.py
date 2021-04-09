af815420ff165fd5352bc7fb5dcefb8953f8e455
netotz <neto.otz@hotmail.com>
2021-04-09 10:12:37 -0500

    Get and save tweets in CSV files

--- src/twitter.py
+++ src/twitter.py
@@ -7,29 +7,32 @@ import nest_asyncio
 
 import constants
 
-def configure_common(limit, since) -> twint.Config:
+def configure_common(
+        limit: int,
+        since: datetime,
+        until: datetime) -> twint.Config:
     '''
     Configures common properties to scrape Twitter.
     '''
-    config = twint.Config()
-    config.Hide_output = True
-    config.Pandas = True
-    config.Pandas_clean = True
-    config.Stats = True
-    config.Limit = limit if limit >= 0 else None
-    config.Since = str(since.date()) if since else None
-    config.Store_csv = True
-
-    return config
+    return twint.Config(
+        Hide_output=True,
+        Pandas=True,
+        Pandas_clean=True,
+        Stats=True,
+        Limit=limit if limit >= 0 else None,
+        Since=str(since.date()) if since else None,
+        Until=str(until.date()) if until else None
+    )
 
 def configure_tweets_by_candidate(
         candidate: constants.Candidate,
         limit: int = -1,
-        since: datetime = None) -> twint.Config:
+        since: datetime = None,
+        until: datetime = None) -> twint.Config:
     '''
     Configures scrapper to get tweets made by a candidate.
     '''
-    config = configure_common(limit, since)
+    config = configure_common(limit, since, until)
     config.Username = candidate.twitter
     config.Filter_retweets = True
 
@@ -38,11 +41,12 @@ def configure_tweets_by_candidate(
 def configure_tweets_by_hashtags(
         hashtags: Sequence[str],
         limit: int = -1,
-        since: datetime = None) -> twint.Config:
+        since: datetime = None,
+        until: datetime = None) -> twint.Config:
     '''
     Configures scrapper to get tweets that include any of the specified hashtags.
     '''
-    config = configure_common(limit, since)
+    config = configure_common(limit, since, until)
     query = ' OR '.join(hashtags)
     config.Custom_query = query
 
@@ -51,13 +55,31 @@ def configure_tweets_by_hashtags(
 # when using IPython
 nest_asyncio.apply()
 
-SINCE = datetime(2021, 3, 5)
-HASHTAGS = ('#ladysecta', '#abelguerra')
-config = configure_tweets_by_hashtags(HASHTAGS, since=SINCE)
+# UNTIL = datetime(2021, 4, 9)
+HASHTAGS = (
+    '#ladysecta',
+    '#claraluzmiente',
+    '#claraluzcensura'
+)
+config = configure_tweets_by_hashtags(
+    HASHTAGS,
+    since=constants.CAMPAIGN_START
+)
 twint.run.Search(config)
 
 # save scrapped tweets as dataframe
-tweets = twint.storage.panda.Tweets_df
+tweets_hashtags = twint.storage.panda.Tweets_df
+tweets_hashtags.to_csv(f'..\\tweets_{HASHTAGS}.csv')
+
+# config = configure_tweets_by_candidate(
+#     constants.CLARA_LUZ,
+#     since=constants.CAMPAIGN_START
+# )
+# twint.run.Search(config)
+
+# tweets_candidate = twint.storage.panda.Tweets_df
+# tweets_candidate.to_csv(f'..\\tweets_{constants.CLARA_LUZ.twitter}.csv')
+
 # # get useful columns by inspecting the dataframe
 # columns_indexes = [0, 3, 22, 23, 24]
 # # filter dataframe by useful columns

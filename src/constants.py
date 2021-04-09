ebbdce442f456d0e7fb9fa2dac5631731376f747
netotz <neto.otz@hotmail.com>
2021-04-09 10:12:02 -0500

    Add relevant events as datetime

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,9 @@
 
+from datetime import datetime
+
 from models.candidate import Candidate
 
 CLARA_LUZ = Candidate('Clara Luz Flores', 'claraluzflores')
+
+CAMPAIGN_START = datetime(2021, 3, 5)
+VIDEO_PUBLISHED = datetime(2021, 3, 24)

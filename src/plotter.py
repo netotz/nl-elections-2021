218b923569c67285547fbca264c13e1327879a28
netotz <neto.otz@hotmail.com>
2021-04-09 18:55:53 -0500

    Add function to plot likes

--- src/plotter.py
+++ src/plotter.py
@@ -7,6 +7,7 @@ import matplotlib.dates as mdates
 import pandas as pd
 
 import constants
+from data_processing import count_likes_sums_by_date
 
 locale.setlocale(locale.LC_TIME, 'es_MX')
 
@@ -23,22 +24,56 @@ def plot_timestamp(timestamp: datetime, label: str, color: str) -> None:
     )
 
 #%%
+def format_date() -> None:
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%b'))
+    plt.gcf().autofmt_xdate()
+
+#%%
 def plot_tweets_per_hashtags(counts: pd.DataFrame) -> None:
     fig, ax  = plt.subplots()
     xdates = counts['date']
+
+    ax.grid(True)
+
     for hashtag in list(counts.columns[1:]):
         ycount = counts[hashtag]
-        ax.plot(xdates, ycount, label=f'#{hashtag}', linewidth='0.8')
-
+        ax.plot(xdates, ycount, label=f'#{hashtag}', linewidth=0.8)
     plot_timestamp(constants.VIDEO_PUBLISHED, 'Video publicado', 'indianred')
 
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b'))
-    ax.grid(True)
     ax.legend()
-
-    fig.autofmt_xdate()
-
+    
+    format_date()
     plt.xlabel('Día')
     plt.ylabel('Tweets')
     plt.title('Cantidad de tweets por hashtag por día')
     plt.show()
+
+#%%
+def plot_likes(tweets: pd.DataFrame) -> None:
+    xdates = tweets['only_date']
+    ylikes = tweets['nlikes']
+
+    plt.grid(True)
+    plt.scatter(
+        xdates, ylikes,
+        s=10, c=ylikes, cmap='YlOrRd',
+        edgecolor='black', linewidth=0.3,
+        alpha=0.8
+    )
+    
+    likes = count_likes_sums_by_date(tweets)
+    xdates = likes['only_date']
+    ysum = likes['sum']
+    plt.plot(xdates, ysum, label='Likes acumulados', linewidth=0.8)
+
+    plot_timestamp(constants.VIDEO_PUBLISHED, 'Video publicado', 'indianred')
+    format_date()
+
+    plt.xlabel('Día')
+    plt.ylabel('Likes')
+    plt.colorbar()
+    plt.legend()
+    plt.show()
+
+
+# %%
